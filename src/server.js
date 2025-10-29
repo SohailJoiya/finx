80ae64ac85d9132acad5a09ad6bc1bc008e0243d
Sohail Amjad <sohail@knockknockapp.ai>
2025-10-29 11:50:51 +0500

    update

--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+//app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'))
